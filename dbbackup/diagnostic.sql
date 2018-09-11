293aa2a90e0b3f200727660f5ee8ae9618a22e2e
kamrul <kamrul_bd_islam@yahoo.com>
2018-09-11 10:19:30 +0600

    db backup sql file included

---
+++ dbbackup/diagnostic.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 11, 2018 at 06:18 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.0.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diagnostic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `appointment_no` int(30) NOT NULL,
+  `patient_id` int(30) NOT NULL,
+  `speciality` varchar(30) NOT NULL,
+  `medical_condition` text,
+  `doctors_suggestion` varchar(30) DEFAULT NULL,
+  `payment_amount` int(199) DEFAULT NULL,
+  `case_closed` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clerks`
+--
+
+CREATE TABLE `clerks` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores information about clerk';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `speciality` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_info`
+--
+
+CREATE TABLE `patient_info` (
+  `patient_Id` int(20) NOT NULL,
+  `full_name` varchar(20) NOT NULL,
+  `DOB` int(10) NOT NULL,
+  `weight` int(8) NOT NULL,
+  `phone_no` varchar(30) NOT NULL,
+  `address` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='patient';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_info`
+--
+
+CREATE TABLE `users_info` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT '2',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users_info`
+--
+
+INSERT INTO `users_info` (`user_id`, `fullname`, `email`, `password`, `role`, `date`) VALUES
+(1, 'dadmin', 'dadmin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1, '2018-09-04 23:49:54');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`appointment_no`);
+
+--
+-- Indexes for table `clerks`
+--
+ALTER TABLE `clerks`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `patient_info`
+--
+ALTER TABLE `patient_info`
+  ADD PRIMARY KEY (`patient_Id`);
+
+--
+-- Indexes for table `users_info`
+--
+ALTER TABLE `users_info`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `appointment_no` int(30) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `patient_info`
+--
+ALTER TABLE `patient_info`
+  MODIFY `patient_Id` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users_info`
+--
+ALTER TABLE `users_info`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
