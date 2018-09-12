8b06998c01c1857a3ee0533b697e1e0ccce06d6c
ThemeExpress <kkislam33@gmail.com>
2018-09-12 16:26:06 +0600

    User registration form included
    User registration form included

--- user_register.php
+++ user_register.php
@@ -1,12 +1,9 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>User | Log in</title>
+  <title>User | Registration Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -19,7 +16,6 @@ session_start();
   <link rel="stylesheet" href="admin/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
   <link rel="stylesheet" href="admin/plugins/iCheck/square/blue.css">
-  <link rel="stylesheet" href="admin/dist/css/adminstyle.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -31,60 +27,61 @@ session_start();
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="login.php"><img src="img/logo.png" ></a>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="user_register.php"><img src="img/logo.png"></a>
   </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-  <?php if (isset($_SESSION['msg'])): ?>
-                        <div class="alert alert-warning">
-                            <?php echo $_SESSION['msg']; ?>
-                        </div>
-                        <?php
-                        unset($_SESSION["msg"]);
-                    endif;
-                    ?>
-    <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="login_process.php" method="post">
+  <div class="register-box-body">
+    <p class="login-box-msg">Register a new membership</p>
+
+    <form action="../../index.html" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" name="email" placeholder="Email">
+        <input type="text" class="form-control" placeholder="Full name">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" name="password" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Retype password">
+        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox"> Remember Me
+              <input type="checkbox"> I agree to the <a href="#">terms</a>
             </label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat" name="user_login">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
         </div>
         <!-- /.col -->
       </div>
     </form>
 
     <div class="social-auth-links text-center">
-      <p>- OR -</p>     
-      <a href="user_register.php" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-user-plus"></i> Register a new membership</a>
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign up using
+        Google+</a>
     </div>
-    <!-- /.social-auth-links -->
 
-    <a href="forget_password.php">I forgot my password</a><br>   
-
+    <a href="login.html" class="text-center">I already have a membership</a>
   </div>
-  <!-- /.login-box-body -->
+  <!-- /.form-box -->
 </div>
-<!-- /.login-box -->
+<!-- /.register-box -->
 
 <!-- jQuery 3 -->
 <script src="admin/bower_components/jquery/dist/jquery.min.js"></script>
